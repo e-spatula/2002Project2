5a57239cba4e7db8a0adefc315a1a83e0ea8c05c
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-10-03 13:34:01 +0800

    adds test file to make

---
+++ test_file.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sifs.h"
+
+//  Written by Chris.McDonald@uwa.edu.au, September 2019
+
+//  REPORT HOW THIS PROGRAM SHOULD BE INVOKED
+int main(int argcount, char *argvalue[])
+{
+    return(0);
+}
